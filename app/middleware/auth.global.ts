fbb288bed60aa7aa5651c7d1086a9ff201850c54
baiwumm <me@baiwumm.com>
2026-03-23 17:39:12 +0800

    fix: 修复 useFetch 引发的错误

--- app/middleware/auth.global.ts
+++ app/middleware/auth.global.ts
@@ -2,12 +2,20 @@
  * @Author: 白雾茫茫丶<baiwumm.com>
  * @Date: 2026-03-18 17:28:20
  * @LastEditors: 白雾茫茫丶<baiwumm.com>
- * @LastEditTime: 2026-03-23 15:50:37
+ * @LastEditTime: 2026-03-23 17:38:03
  * @Description: 认证鉴权
  */
 export default defineNuxtRouteMiddleware(async (to) => {
   const { $authClient } = useNuxtApp()
-  const { data: session } = await $authClient.useSession(useFetch)
+  const relativeFetch = ((url: string, opts?: any) => {
+    try {
+      if (url.startsWith('http'))
+        url = new URL(url).pathname
+    }
+    catch {}
+    return useFetch(url, opts)
+  }) as any
+  const { data: session } = await $authClient.useSession(relativeFetch)
   const isLoggedIn = !!session.value
 
   // 未登录访问私有页面
