8cee59c3ada9901f23c322507dad5e171f3d0b3d
baiwumm <me@baiwumm.com>
2026-03-24 17:35:17 +0800

    feat(components): 新增 Spinner 组件

--- app/pages/dashboard/index.vue
+++ app/pages/dashboard/index.vue
@@ -22,13 +22,68 @@ async function signOut() {
 </script>
 
 <template>
-  <div>
-    <UButton>{{ $t('pages.dashboard.title') }}</UButton>
-    <UButton :loading="session.isPending">
-      {{ session?.data?.user?.name }}
-    </UButton>
-    <UButton :loading @click="signOut">
-      注销
-    </UButton>
+  <div class="w-screen h-screen flex justify-center items-center flex-col gap-4">
+    <div class="flex items-center gap-4">
+      <UButton>{{ $t('pages.dashboard.title') }}</UButton>
+      <UAvatar
+        :text="session?.data?.user?.name || session?.data?.user?.email[0]"
+        :chip="{
+          inset: true,
+          position: 'bottom-right',
+          color: 'success',
+        }"
+      />
+      <UButton :loading @click="signOut">
+        注销
+      </UButton>
+    </div>
+    <div class="flex items-center gap-8">
+      <div class="flex flex-col items-center gap-2">
+        <Spinner />
+        <span class="text-xs text-muted">Primary</span>
+      </div>
+      <div class="flex flex-col items-center gap-2">
+        <Spinner color="secondary" />
+        <span class="text-xs text-muted">Secondary</span>
+      </div>
+      <div class="flex flex-col items-center gap-2">
+        <Spinner color="success" />
+        <span class="text-xs text-muted">Success</span>
+      </div>
+      <div class="flex flex-col items-center gap-2">
+        <Spinner color="info" />
+        <span class="text-xs text-muted">Info</span>
+      </div>
+      <div class="flex flex-col items-center gap-2">
+        <Spinner color="warning" />
+        <span class="text-xs text-muted">Warning</span>
+      </div>
+      <div class="flex flex-col items-center gap-2">
+        <Spinner color="error" />
+        <span class="text-xs text-muted">Error</span>
+      </div>
+      <div class="flex flex-col items-center gap-2">
+        <Spinner color="neutral" />
+        <span class="text-xs text-muted">Neutral</span>
+      </div>
+    </div>
+    <div class="flex items-center gap-8">
+      <div class="flex flex-col items-center gap-2">
+        <Spinner size="sm" />
+        <span class="text-xs text-muted">Small</span>
+      </div>
+      <div class="flex flex-col items-center gap-2">
+        <Spinner size="md" />
+        <span class="text-xs text-muted">Medium</span>
+      </div>
+      <div class="flex flex-col items-center gap-2">
+        <Spinner size="lg" />
+        <span class="text-xs text-muted">Large</span>
+      </div>
+      <div class="flex flex-col items-center gap-2">
+        <Spinner size="xl" />
+        <span class="text-xs text-muted">Extra Large</span>
+      </div>
+    </div>
   </div>
 </template>
